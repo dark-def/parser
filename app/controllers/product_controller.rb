dc69e8fc524fa5cc811adf33f392e14b6e4f7f70
dark-def <drkmen@rambler.ru>
2013-08-28 19:38:13 +0300

    try

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -12,6 +12,7 @@ class ProductController < ApplicationController
 
     require 'selenium-webdriver'
     require 'nokogiri'
+    require 'open-uri'
 
     driver = Selenium::WebDriver.for :firefox
     driver.manage.window.maximize
@@ -23,38 +24,23 @@ class ProductController < ApplicationController
     doc.css("img").each_with_index do |item, index|
       @images[index] = item['src']
     end
-    #element.location_once_scrolled_into_view
+
     driver.find_element(:xpath, '/html/body/div/div/header/div/ul/li[5]/a').click
-    driver.find_element(:xpath, '/html/body/div/div/div[2]/div/ul/li[3]/a').click
+    driver.find_element(:xpath, '/html/body/div/div/div[2]/div/ul/li[3]/a').click         #ruby on rails
     driver.find_element(:xpath, '/html/body/div/div/div[2]/div[2]/div/div[2]/div[5]/h2/a').location_once_scrolled_into_view
     driver.find_element(:xpath, '/html/body/div/div/div[2]/div[2]/div/div[2]/div[5]/h2/a').click
     doc = Nokogiri::HTML(driver.page_source)
     @post = doc.xpath('/html/body/div/div/div[2]/div[2]/div')
-    #@post = driver.find_element(:class, 'post')
-    #element = driver.find_element(:name, 'text')
-    #element.send_keys 'http://faceit-team.com/'
-    #element.submit
 
-
-    #element = driver.find_element(:id, "coolestWidgetEvah")
-    #link = driver.find_element(:xpath, '/html/body/div[2]/div[2]/div/div[2]/ol/li/h2/a')
-    #link.click
-    #driver.find_element(:partial_link_text, 'Knowledge').click
-    #element.click
-    #element = driver.find_element(:link_text, 'Ruby on Rails FAQ')
-    #element.click.find_element(:link, 'Ruby on Rails FAQ')
+    driver.find_element(:xpath, '/html/body/div/div/header/div/ul/li[5]/a').click         #ruby on rails
+    driver.find_element(:xpath, '/html/body/div/div/div[2]/div[2]/div/div[2]/div[6]/h2/a').location_once_scrolled_into_view
+    driver.find_element(:xpath, '/html/body/div/div/div[2]/div[2]/div/div[2]/div[6]/h2/a').click
+    doc = Nokogiri::HTML(driver.page_source)
+    @post2 = doc.xpath('/html/body/div/div/div[2]/div[2]/div')
 
     puts driver.title
-    #b-serp-list i-bem b-serp-list_js_inited
-    #element = driver.find_element(:li)
 
     driver.quit
 
-
-    #doc = Nokogiri::HTML(driver.page_source)
-    #@logo = doc.css('hplogo')
-    #doc.css("img").each_with_index do |item, index|
-    #  @images[index] = item['src']
-    #end
   end
 end
